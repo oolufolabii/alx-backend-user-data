a1d7b72c8ded0f95e49f85f64dd36f6da0311ae6
oolufolabii <otitoola.olufolabi@gmail.com>
2023-03-15 17:52:58 +0100

    User Authentication

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,32 +3,27 @@
 """DB module
 """
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-
 from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
-
+from typing import TypeVar
 from user import Base, User
 
 
 class DB:
-    """DB class
-    """
+    """ class """
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+    def __init__(self):
+        """ constructor """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
-        """
+    def _session(self):
+        """ create a session """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
@@ -36,12 +31,11 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Adding a new user to the database"""
-   
-        create_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(create_user)
+
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
-        
-        return create_user
+        return user
 
     def find_user_by(self, **kwargs) -> User:
         """Finds a user based on a set of filters.
@@ -56,9 +50,9 @@ class DB:
     def update_user(self, user_id: int, **kwargs) -> None:
         """method that takes as argument a required user_id integer and arbitrary
         keyword arguments, and returns None"""
-        user = self.find_user_by(id=user_id)
+        _id = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
-            if not hasattr(user, key):
+            if not hasattr(_id, key):
                 raise ValueError
-            setattr(user, key, value)
+            setattr(_id, key, value)
         self._session.commit()
